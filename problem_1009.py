82f878baf92459c5562713bf290f50b4dbc900ec
nauta <contatofernando@protonmail.com>
2022-09-14 23:15:26 -0300

    Problem 1009 accepted

---
+++ problem_1009.py
@@ -0,0 +1,6 @@
+name = input('')
+fixed_salary = float(input(''))
+total_sale = float(input(''))
+final_salary = fixed_salary + total_sale * 0.15
+print('TOTAL = R$ {:.2f}'.format(final_salary))
+                     
